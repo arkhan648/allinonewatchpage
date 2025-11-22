26d69eeaf81705247dda3fcfb7a7290ff03688c1
arkhan648 <129271861+arkhan648@users.noreply.github.com>
2025-11-22 13:40:23 +0600

    Update watch.js

--- watch.js
+++ watch.js
@@ -35,7 +35,7 @@ const OFFERS_BY_COUNTRY = {
 };
 
 const CROSS_BORDER_RULES = {
-    "https://1wksrw.com/betting?open=register&p=xctu": ["BR", "RU", "IN"], 
+    "https://1wksrw.com/betting?open=register&p=xctu": ["BR", "RU", "IN", "GR"], 
     "https://1wksrw.com/?open=register&p=h8zt": ["BD", "PH", "AR"],
     "https://record.betsson.com/_Ipto0Q-i5zR7HLc7-ZUbAGNd7ZgqdRLk/1/": ["AR", "BR", "CO", "EE", "ES", "KZ", "LV", "LT", "MX", "CL", "PE", "RS"],
     "https://record.webpartners.co/_QUm2k2WIfIo1bkozPnn1SmNd7ZgqdRLk/1/": ["DE", "NO", "NZ", "PR", "PT", "AR", "CL", "PH", "SG", "HR"]
@@ -357,6 +357,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener('hashchange', initializeWatchPage);
+
 
 
 
